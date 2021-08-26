7d780520a99d9097365de787f2cfdc4c859a9209
DilshadGhumman <ghummandilshad@gmail.com>
2021-08-26 14:26:50 -0400

    CSS Bugs Fixing

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,64 +5,66 @@
 <main class="mt-3">
     <section class="widget-contact position-relative" id="contact-us">
         <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <div class="circle pt-5">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                    </div>
-                    <div class="d-flex align-items-center pb-4 pt-2">
-                        <h3 class="secondary_color_text tag-title m-0 fw-500">Contact</h3>
-                        <h3 class="primary_color_text tag-title m-0 ps-2 fw-500">Support</h3>
-                    </div>
-                    <p class="linehight_19 font_16 grey pe-5 roboto">
-                        Please fill out this form and one of our customer support agents will follow-up and promptly assist you.
-                    </p>
-                    <p class="linehight_19 font_16 grey pe-5 roboto">
-                        You will receive an email from our support system after you submit this form. Once you receive the acknowledgement email, you can reply to that email and continue the conversation with the support agent.</p>
-                </div>
-                <div class="col-12 col-md-6">
-                    <form class="contact-form widget-shadow">
-                        <div class="row">
-                            <div class="col-12 mb-3">
-                                <label for="exampleInputFirstName" class="form-label linehight_19 font_16 grey ps-3">First
-                                    Name*</label>
-                                <input type="Fname" class="form-control" id="exampleInputFirstName" aria-describedby="NameHelp" name="first_name" required>
-                            </div>
-                            <div class="col-12 mb-3">
-                                <label for="exampleInputEmail" class="form-label linehight_19 font_16 grey ps-3">Email*</label>
-                                <input type="Email" class="form-control" id="exampleInputEmail" name="last_name" required>
-                            </div>
-                            <div class="col-12 col-md-6 mb-3">
-                                <label for="exampleInputPhone" class="form-label linehight_19 font_16 grey ps-3">Mobile
-                                    Phone*</label>
-                                <input type="text" class="form-control" id="exampleInputPhone" aria-describedby="PhoneHelp" name="mobile" required>
-                            </div>
-                            <div class="col-12 col-md-6 mb-3">
-                                <div class="form-group">
-                                    <label for="exampleInputfrequency" class="form-label linehight_19 font_16 grey ps-3">Frequency*</label>
-                                    <span class="form-input">
-                                        <select class="form-control" id="" name="frequency" required>
-                                            <option>Choose One</option>
-                                            <option>2</option>
-                                            <option>3</option>
-                                            <option>4</option>
-                                        </select>
-                                        <img src="images/caret.svg" class="img-fluid caret-icon"
-                                            alt="User icon">
-                                    </span>
-                                </div>
-                            </div>
-                            <div class="col-12 mb-4">
-                                <div class="form-group">
-                                    <label for="exampleFormControlTextarea1" class="linehight_19 font_16 grey ps-3">Description</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
-                                </div>
-                            </div>
-                            <button type="button" class="btn ms-auto text-white font_20 linehight_23 b-100-radious md-btn secondary_color_bg create-ticket">Create Ticket</button>
+            <div class="contact-form widget-shadow c_support">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <div class="circle">
+                            <img class="img-fluid" src="/images/ellipse.png">
+                            <img class="img-fluid" src="/images/ellipse.png">
+                            <img class="img-fluid" src="/images/ellipse.png">
                         </div>
-                    </form>
+                        <div class="d-flex align-items-center pb-4 pt-2">
+                            <h3 class="secondary_color_text tag-title m-0 fw-500">Contact</h3>
+                            <h3 class="primary_color_text tag-title m-0 ps-2 fw-500">Support</h3>
+                        </div>
+                        <p class="linehight_19 font_16 grey pe-5 roboto">
+                            Please fill out this form and one of our customer support agents will follow-up and promptly assist you.
+                        </p>
+                        <p class="linehight_19 font_16 grey pe-5 roboto">
+                            You will receive an email from our support system after you submit this form. Once you receive the acknowledgement email, you can reply to that email and continue the conversation with the support agent.</p>
+                        <img src="/images/content-s-attr.svg" class="img-fluid m-auto d-flex " alt="attr"> 
+                    </div>
+                    <div class="col-12 col-md-6 mt-5">
+                        <form class="mt-5">
+                            <div class="row g-2">
+                                <div class="col-12 mb-3">
+                                    <label for="exampleInputFulltName" class="form-label linehight_19 font_16 grey ps-4">Full
+                                        Name*</label>
+                                    <input type="Fname" class="form-control ps-4" id="exampleInputFulltName" aria-describedby="NameHelp" name="first_name" required>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <label for="exampleInputEmail" class="form-label linehight_19 font_16 grey ps-4">Email*</label>
+                                    <input type="Email" class="form-control ps-4" id="exampleInputEmail" name="last_name" required>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <label for="exampleInputPhone" class="form-label linehight_19 font_16 grey ps-4">Mobile
+                                        Phone*</label>
+                                    <input type="text" class="form-control ps-4" id="exampleInputPhone" aria-describedby="PhoneHelp" name="mobile" required>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputfrequency" class="form-label linehight_19 font_16 grey ps-4">Frequency*</label>
+                                        <span class="form-input">
+                                            <select class="form-control ps-4" id="" name="frequency" required>
+                                                <option>Choose One</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                            </select>
+                                            <img src="images/caret.svg" class="img-fluid caret-icon" alt="User icon">
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="col-12 mb-4">
+                                    <div class="form-group">
+                                        <label for="exampleFormControlTextarea1" class="linehight_19 font_16 grey ps-4">Description</label>
+                                        <textarea class="form-control ps-4" id="exampleFormControlTextarea1" rows="3" name="description"></textarea>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn ms-auto text-white font_20 linehight_23 b-100-radious md-btn secondary_color_bg create-ticket">Create Ticket</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -71,37 +73,35 @@
 @endsection
 @section('script')
 <script>
-
-$(document).ready(
-    function(){
-
-
-
-        $(document).on('click','.create-ticket',function(){
+    $(document).ready(
+        function() {
 
 
 
-         form_data = $('.get-support-form').serialize();
-         $.ajax({
-            url:"/add-ticket",
-            type:"post",
-            data:form_data,
-            beforeSend:function(){
+            $(document).on('click', '.create-ticket', function() {
 
-            },
-            error: function(response){
-                 window.location.replace("/contact-support-confirmation");
-            },
-            success:function(response){
 
-                window.location.replace("/contact-support-confirmation");
 
-            }
-        });
+                form_data = $('.get-support-form').serialize();
+                $.ajax({
+                    url: "/add-ticket",
+                    type: "post",
+                    data: form_data,
+                    beforeSend: function() {
 
-     })
-    }
+                    },
+                    error: function(response) {
+                        window.location.replace("/contact-support-confirmation");
+                    },
+                    success: function(response) {
+
+                        window.location.replace("/contact-support-confirmation");
+
+                    }
+                });
+
+            })
+        }
     );
-
 </script>
 @endsection
--- resources/views/get-demo.blade.php
+++ resources/views/get-demo.blade.php
@@ -5,75 +5,84 @@
 <main class="mt-3">
     <section id="contact-us">
         <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <div class="circle pt-5">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                        <img class="img-fluid" src="/images/ellipse.png">
-                    </div>
-                    <div class="d-flex align-items-center pb-4 pt-2">
-                        <h3 class="secondary_color_text tag-title m-0 fw-500">Get</h3>
-                        <h3 class="primary_color_text tag-title m-0 ps-2 fw-500">Demo</h3>
-                    </div>
-                    <p class="dark_gray font_16 linehight_19 pe-5 roboto">Please complete and submit this form to receive an email with our demo video.
-                    </p>
-                    <p class="dark_gray font_16 linehight_19 pe-5 roboto">If you have any questions, please do not hesitate to reach out to your support team. You can also request for a demo session with one of our Customer Success team members.
-                    </p>
-                </div>
-                <div class="col-12 col-md-6">
-                    <form class="contact-form widget-shadow widget-contact get-demo-form">
-                        <div class="alert alert-danger error-message"></div>
-                        {{ csrf_field() }}
-                        <input type="hidden" name="DEVICE" class="DEVICE">
-                        <input type="hidden" name="UTCOFFSET" class="UTCOFFSET">
-                        <div class="row">
-                            <div class="col-12 col-md-6 mb-3">
-                                <label for="exampleInputFirstName" class="form-label ps-3 grey font_16 linehight_19">First Name*</label>
-                                <input type="text" class="form-control" id="exampleInputFirstName" aria-describedby="NameHelp" name="FNAME" placeholder="First name" required>
-                            </div>
-                            <div class="col-12 col-md-6 mb-3">
-                                <label for="exampleInputLastName" class="form-label ps-3 grey font_16 linehight_19">Last Name*</label>
-                                <input type="text" class="form-control" id="exampleInputLastName" name="LNAME" placeholder="Last Name" required>
-                            </div>
-                            <div class="col-12 mb-3">
-                                <label for="exampleInputEmail" class="form-label ps-3 grey font_16 linehight_19">Email*</label>
-                                <input type="email" class="form-control" id="exampleInputEmail" name="EMAIL" placeholder="Email" required>
-                            </div>
-                            <div class="col-12 mb-3">
-                                <label for="exampleInputBusiness" class="form-label ps-3 grey font_16 linehight_19">Business Name*</label>
-                                <input type="text" class="form-control" id="exampleInputBusiness" name="COMPNAME" placeholder="Business Name" required>
-                            </div>
-                            <div class="col-12 col-md-6 mb-3">
-                                <label for="exampleInputPhone" class="form-label ps-3 grey font_16 linehight_19">Mobile Phone*</label>
-                                <input type="text" class="form-control" id="exampleInputPhone" aria-describedby="PhoneHelp" name="PHONE" placeholder="Mobile Phone" required>
-                            </div>
-                            <div class="col-12 col-md-6 mb-3">
-                                <div class="form-group">
-                                    <label for="exampleInputClient" class="form-label fo ps-3 grey font_16 linehight_19">Client Count*</label>
-                                    <input type="text" class="form-control" id="exampleInputPhone" aria-describedby="PhoneHelp" name="CLIENTCNT" placeholder="Client Count" required>
-                                </div>
-                            </div>
-                            <div class="col-12 mb-4">
-                                <div class="form-group">
-                                    <label for="review" class="form-label ps-3 grey font_16 linehight_19">How Did You Hear About Us?*</label>
-                                    <span class="form-input">
-                                        <select class="form-control" id="" name="SOURCE" required>
-                                            <option value="Search Engine">Search Engine</option>
-                                            <option value="Social Media">Social Media</option>
-                                            <option value="Family-Friend-Contact">Family-Friend-Contact</option>
-                                            <option value="Podcast">Podcast</option>
-                                            <option value="Other">Other</option>
-                                        </select>
-                                        <img src="images/caret.svg" class="img-fluid caret-icon"
-                                        alt="User icon">
-                                    </span>
-                                </div>
-                            </div>
-                            <button type="button" class="btn ms-auto text-white font_20 linehight_23 b-10-radious md-btn secondary_color_bg get-demo-submit">Get Demo</button>
-
+            <div class="contact-form widget-shadow c_support">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <div class="circle">
+                            <img class="img-fluid" src="/images/ellipse.png">
+                            <img class="img-fluid" src="/images/ellipse.png">
+                            <img class="img-fluid" src="/images/ellipse.png">
                         </div>
-                    </form>
+                        <div class="d-flex align-items-center pb-4 pt-2">
+                            <h3 class="secondary_color_text tag-title m-0 fw-500">Get</h3>
+                            <h3 class="primary_color_text tag-title m-0 ps-2 fw-500">Demo</h3>
+                        </div>
+                        <p class="dark_gray font_16 linehight_19 pe-5 roboto">Please complete and submit this form to receive an email with our demo video.
+                        </p>
+                        <p class="dark_gray font_16 linehight_19 pe-5 roboto">If you have any questions, please do not hesitate to reach out to your support team. You can also request for a demo session with one of our Customer Success team members.
+                        </p>
+                        <img src="/images/content-s-attr.svg" class="img-fluid m-auto d-flex " alt="attr"> 
+                    </div>
+                    <div class="col-12 col-md-6 mt-5">
+                        <form class="mt-5">
+                            <div class="alert alert-danger error-message"></div>
+                            {{ csrf_field() }}
+                            <input type="hidden" name="DEVICE" class="DEVICE">
+                            <input type="hidden" name="UTCOFFSET" class="UTCOFFSET">
+                            <div class="row g-2">
+                                <div class="col-12 col-md-6 mb-3">
+                                    <label for="exampleInputFirstName" class="form-label grey font_16 linehight_19 ps-4">First Name*</label>
+                                    <input type="text" class="form-control ps-4" id="exampleInputFirstName" aria-describedby="NameHelp" name="FNAME" placeholder="First name" required>
+                                </div>
+                                <div class="col-12 col-md-6 mb-3">
+                                    <label for="exampleInputLastName" class="form-label grey font_16 linehight_19 ps-4">Last Name*</label>
+                                    <input type="text" class="form-control ps-4" id="exampleInputLastName" name="LNAME" placeholder="Last Name" required>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <label for="exampleInputEmail" class="form-label grey font_16 linehight_19 ps-4">Email*</label>
+                                    <input type="email" class="form-control ps-4" id="exampleInputEmail" name="EMAIL" placeholder="Email" required>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <label for="exampleInputBusiness" class="form-label grey font_16 linehight_19 ps-4">Business Name*</label>
+                                    <input type="text" class="form-control ps-4" id="exampleInputBusiness" name="COMPNAME" placeholder="Business Name" required>
+                                </div>
+                                <div class="col-12 col-md-6 mb-3">
+                                    <label for="exampleInputPhone" class="form-label grey font_16 linehight_19 ps-4">Mobile Phone*</label>
+                                    <input type="text" class="form-control ps-4" id="exampleInputPhone" aria-describedby="PhoneHelp" name="PHONE" placeholder="Mobile Phone" required>
+                                </div>
+                                <div class="col-12 col-md-6 mb-3">
+                                    <div class="form-group">
+                                        <label for="exampleInputClient" class="form-label grey font_16 linehight_19 ps-4">Client Count*</label>
+                                        <span class="form-input">
+                                            <select class="form-control linehight_19 font_16 ps-4" id="" name="CLIENTCNT" required>
+                                                <option selected disabled value="">Choose One</option>
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>123</option>
+                                            </select>
+                                            <img src="images/caret.svg" class="img-fluid caret-icon" alt="User icon">
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="col-12 mb-4">
+                                    <div class="form-group">
+                                        <label for="review" class="form-label ps-3 grey font_16 linehight_19 ps-4">How Did You Hear About Us*</label>
+                                        <span class="form-input">
+                                            <select class="form-control ps-4" id="" name="SOURCE" required>
+                                            <option selected disabled value="">Choose One</option>
+                                                <option value="Social Media">Social Media</option>
+                                                <option value="Family-Friend-Contact">Family-Friend-Contact</option>
+                                                <option value="Podcast">Podcast</option>
+                                                <option value="Other">Other</option>
+                                            </select>
+                                            <img src="images/caret.svg" class="img-fluid caret-icon" alt="User icon">
+                                        </span>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn ms-auto text-white font_20 linehight_23 b-100-radious md-btn secondary_color_bg create-ticket">Request Demo</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,7 +91,7 @@
 @endsection
 @section('script')
 <script>
-    function detectWindow(){
+    function detectWindow() {
         var detectOS = "Unknown OS";
 
         if (navigator.appVersion.indexOf("Win") != -1)
@@ -98,67 +107,66 @@
     }
 
     function detectBrowser() {
-        if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
+        if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
             return 'Opera';
-        } else if(navigator.userAgent.indexOf("Chrome") != -1 ) {
+        } else if (navigator.userAgent.indexOf("Chrome") != -1) {
             return 'Chrome';
-        } else if(navigator.userAgent.indexOf("Safari") != -1) {
+        } else if (navigator.userAgent.indexOf("Safari") != -1) {
             return 'Safari';
-        } else if(navigator.userAgent.indexOf("Firefox") != -1 ){
+        } else if (navigator.userAgent.indexOf("Firefox") != -1) {
             return 'Firefox';
-        } else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {
-        return 'IE';//crap
-    } else {
-        return 'Unknown';
+        } else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) {
+            return 'IE'; //crap
+        } else {
+            return 'Unknown';
+        }
     }
-}
-$(document).ready(
-    function(){
+    $(document).ready(
+        function() {
 
 
-        $('.error-message').hide();
-        $('.DEVICE').val(detectWindow());
-        var offset = detectBrowser()/60;
-        $('.UTCOFFSET').val(offset);
-        $(document).on('click','.get-demo-submit',function(){
+            $('.error-message').hide();
+            $('.DEVICE').val(detectWindow());
+            var offset = detectBrowser() / 60;
+            $('.UTCOFFSET').val(offset);
+            $(document).on('click', '.get-demo-submit', function() {
 
-            FNAME = $('input[name="FNAME"]').val();
-            LNAME = $('input[name="LNAME"]').val();
-            EMAIL = $('input[name="EMAIL"]').val();
-            COMPNAME = $('input[name="COMPNAME"]').val();
-            PHONE = $('input[name="PHONE"]').val();
-            CLIENTCNT = $('input[name="CLIENTCNT"]').val();
-            SOURCE = $('select[name=SOURCE] option').filter(':selected').val();
+                FNAME = $('input[name="FNAME"]').val();
+                LNAME = $('input[name="LNAME"]').val();
+                EMAIL = $('input[name="EMAIL"]').val();
+                COMPNAME = $('input[name="COMPNAME"]').val();
+                PHONE = $('input[name="PHONE"]').val();
+                CLIENTCNT = $('input[name="CLIENTCNT"]').val();
+                SOURCE = $('select[name=SOURCE] option').filter(':selected').val();
 
-            if( FNAME == '' || LNAME == '' || EMAIL == '' || COMPNAME == '' || PHONE =='' || CLIENTCNT == '' || SOURCE == '' ){
-             $('.error-message').show();
-             $('.error-message').html('Please fill all fields');
-             return false;
-         }
+                if (FNAME == '' || LNAME == '' || EMAIL == '' || COMPNAME == '' || PHONE == '' || CLIENTCNT == '' || SOURCE == '') {
+                    $('.error-message').show();
+                    $('.error-message').html('Please fill all fields');
+                    return false;
+                }
 
-         form_data = $('.get-demo-form').serialize();
-         $.ajax({
-            url:"/mailchimp-suscribe",
-            type:"post",
-            data:form_data,
-            beforeSend:function(){
+                form_data = $('.get-demo-form').serialize();
+                $.ajax({
+                    url: "/mailchimp-suscribe",
+                    type: "post",
+                    data: form_data,
+                    beforeSend: function() {
 
-            },
-            error: function(response){
+                    },
+                    error: function(response) {
 
-                $('.error-message').show();
-                $('.error-message').html('Please fill all fields');
-            },
-            success:function(response){
+                        $('.error-message').show();
+                        $('.error-message').html('Please fill all fields');
+                    },
+                    success: function(response) {
 
-                window.location.replace("/getdemo-confirmation");
+                        window.location.replace("/getdemo-confirmation");
 
-            }
-        });
+                    }
+                });
 
-     })
-    }
+            })
+        }
     );
-
 </script>
 @endsection
